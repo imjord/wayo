d876f073bd1c9c231444dcd4709e692a5a5ec683
Juan Hernandez <juanh22@gmail.com>
2021-09-21 20:24:26 -0600

    all models up

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require()
+
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        password: {
+            type: String,
+            require: true,
+            minlength: 6
+        },
+    },
+);
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
