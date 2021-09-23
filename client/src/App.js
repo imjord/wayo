c8288586354de3c107892182cc2146624d6967af
imjord <dean.jay.goat@gmail.com>
2021-09-22 18:25:57 -0600

    able to run both server and client same time with npm run develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div class="lists">
-  <ul class="nes-list is-disc">
-    <li>Good morning.</li>
-    <li>Thou hast had a good night's sleep, I hope.</li>
-    <li>Thou hast had a good afternoon</li>
-    <li>Good night.</li>
-  </ul>
-</div>
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
-<div class="lists">
-  <ul class="nes-list is-circle">
-    <li>Good morning.</li>
-    <li>Thou hast had a good night's sleep, I hope.</li>
-    <li>Thou hast had a good afternoon</li>
-    <li>Good night.</li>
-  </ul>
-</div>
-      </header>
-    </div>
+
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+      <div>
+      <Switch>
+        
+          </Switch>
+      </div>
+      </Router>
+    </ApolloProvider>
+   
   );
 }
 
