7d59fb4e5bfc0570d11b741bea947307591e1cb9
imjord <dean.jay.goat@gmail.com>
2021-09-24 14:15:34 -0600

    working on seeder file

---
+++ server/config/seeds.js
@@ -0,0 +1,6 @@
+const db = require('./connection') // connect to our mondb db file
+const { User, Products } = require('../models'); // import our models that juan boi made
+
+db.once('open', async () => {
+    await Category,deleteMant
+})
