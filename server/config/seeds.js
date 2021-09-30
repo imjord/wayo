182bc9a2ef3075b549c6adf0c6ffec232eee3307
imjord <dean.jay.goat@gmail.com>
2021-09-29 18:28:33 -0600

    sseded more stuff

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -6,7 +6,9 @@ db.once('open', async () => {
     // insert name strings into the categories model using insermany function
     const myCategories = await Categories.insertMany([
         {name: 'T-Shirts'},
-        {name: 'Hoodies'}
+        {name: 'Hoodies'},
+        {name: 'Leggings'},
+        {name: 'Flip-flops'}
     ]);
 
     console.log('Categories done');
@@ -27,8 +29,63 @@ db.once('open', async () => {
             category: myCategories[1]._id,
             price: 25.99,
             image: 'HOODIE.PNG'
-        }    
-           
+        },  
+        {
+            name: 'Wayo Leggings',
+            description: 'Some awesome Leggings for anyone!',
+            category: myCategories[2]._id,
+            price: 5.99,
+            image: '.../client/src/assets/images/WAYO_LEGGINGS.PNG'
+        },  
+        {
+            name: 'Wayo Flip Flops',
+            description: 'Get ur summer on in these sweet flops!',
+            category: myCategories[3]._id,
+            price: 2.99,
+            image: '.../client/src/assets/images/WAYO_FLIPFLOPS.PNG'
+        }, 
+        {
+            name: 'Wayo rare Hoodie',
+            description: 'Deadstock only 1 left',
+            category: myCategories[1]._id,
+            price: 420.69,
+            image: '.../client/src/assets/images/WAYO_RARE_HOODIE.PNG'
+        }, 
+        {
+            name: 'Wayo RARE Hoodie',
+            description: 'Deadstock no more left',
+            category: myCategories[1]._id,
+            price: 250.99,
+            image: '.../client/src/assets/images/WAYO_RARE01.PNG'
+        },    
+        {
+            name: 'Wayo SPORTS',
+            description: 'AWESOME SPORTS TOP YA!',
+            category: myCategories[0]._id,
+            price: 25.99,
+            image: '.../client/src/assets/images/WAYO_SPORT.PNG'
+        },  
+        {
+            name: 'Wayo PINK SHIRT WOOOO',
+            description: 'A NICE SHIRT',
+            category: myCategories[0]._id,
+            price: 25.99,
+            image: '.../client/src/assets/images/WAYO_PINK.PNG'
+        },  
+        {
+            name: 'Wayo Professional shirt',
+            description: 'wayo but upside down. classy. very classy.',
+            category: myCategories[0]._id,
+            price: 89.99,
+            image: '.../client/src/assets/images/WAYO_PROFESSIONAL.PNG'
+        },  
+        {
+            name: 'Wayo Blue long sleeve shirt',
+            description: 'pretty rare so its expensive ',
+            category: myCategories[0]._id,
+            price: 109.99,
+            image: '.../client/src/assets/images/WAYO_02.PNG'
+        },  
     
     ]);
     console.log('products doine')
