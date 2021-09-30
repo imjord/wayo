4d45d878e5a301faa9f8d8a0712c752cf02fac03
imjord <dean.jay.goat@gmail.com>
2021-09-30 17:26:21 -0600

    staticheroku

--- server/server.js
+++ server/server.js
@@ -33,12 +33,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve up static assets for heroku i think will need later
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//   }
-//   app.get('*', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 
 db.once('open', () => {
