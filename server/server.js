6fca728de3a6b882391fe414757bc826c3b74eac
imjord <dean.jay.goat@gmail.com>
2021-10-03 10:40:30 -0600

    yes

--- server/server.js
+++ server/server.js
@@ -32,15 +32,6 @@ startServer(); // start apollo server boi
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-<<<<<<< HEAD
-// serve up static assets for heroku i think will need later
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-  app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-=======
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
@@ -51,7 +42,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
->>>>>>> develop
 
 
 db.once('open', () => {
